98019e35bcb0e09694bfe4a011628981ad92e873
Mincho Paskalev <minchopaskal@gmail.com>
2025-11-10 12:56:13 +0200

    fix prints

--- tests/modules/auth.c
+++ tests/modules/auth.c
@@ -10,6 +10,62 @@
 
 #define UNUSED(V) ((void) V)
 
+#include <time.h>
+
+static int is_leap_year(time_t year) {
+    if (year % 4) return 0;         /* A year not divisible by 4 is not leap. */
+    else if (year % 100) return 1;  /* If div by 4 and not 100 is surely leap. */
+    else if (year % 400) return 0;  /* If div by 100 *and* not by 400 is not leap. */
+    else return 1;                  /* If div by 100 and 400 is leap. */
+}
+
+void nolocks_localtime(struct tm *tmp, time_t t, time_t tz, int dst) {
+    const time_t secs_min = 60;
+    const time_t secs_hour = 3600;
+    const time_t secs_day = 3600*24;
+
+    t -= tz;                            /* Adjust for timezone. */
+    t += 3600*dst;                      /* Adjust for daylight time. */
+    time_t days = t / secs_day;         /* Days passed since epoch. */
+    time_t seconds = t % secs_day;      /* Remaining seconds. */
+
+    tmp->tm_isdst = dst;
+    tmp->tm_hour = seconds / secs_hour;
+    tmp->tm_min = (seconds % secs_hour) / secs_min;
+    tmp->tm_sec = (seconds % secs_hour) % secs_min;
+
+    /* 1/1/1970 was a Thursday, that is, day 4 from the POV of the tm structure
+     * where sunday = 0, so to calculate the day of the week we have to add 4
+     * and take the modulo by 7. */
+    tmp->tm_wday = (days+4)%7;
+
+    /* Calculate the current year. */
+    tmp->tm_year = 1970;
+    while(1) {
+        /* Leap years have one day more. */
+        time_t days_this_year = 365 + is_leap_year(tmp->tm_year);
+        if (days_this_year > days) break;
+        days -= days_this_year;
+        tmp->tm_year++;
+    }
+    tmp->tm_yday = days;  /* Number of day of the current year. */
+
+    /* We need to calculate in which month and day of the month we are. To do
+     * so we need to skip days according to how many days there are in each
+     * month, and adjust for the leap year that has one more day in February. */
+    int mdays[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    mdays[1] += is_leap_year(tmp->tm_year);
+
+    tmp->tm_mon = 0;
+    while(days >= mdays[tmp->tm_mon]) {
+        days -= mdays[tmp->tm_mon];
+        tmp->tm_mon++;
+    }
+
+    tmp->tm_mday = days+1;  /* Add 1 since our 'days' is zero-based. */
+    tmp->tm_year -= 1900;   /* Surprisingly tm_year is year-1900. */
+}
+
 // A simple global user
 static RedisModuleUser *global = NULL;
 static long long client_change_delta = 0;
@@ -116,12 +172,23 @@ int test_rm_register_auth_cb(RedisModuleCtx *ctx, RedisModuleString **argv, int 
  * `AuthBlock_Reply`.
  * `arg` is expected to contain the RedisModuleBlockedClient, username, and password.
  */
+long getTimeZone(void) {
+#if defined(__linux__) || defined(__sun)
+    return timezone;
+#else
+    struct timezone tz;
+
+    gettimeofday(NULL, &tz);
+
+    return tz.tz_minuteswest * 60L;
+#endif
+}
+
 void *AuthBlock_ThreadMain(void *arg) {
     char buf[64];
     gettimeofday(&tv,NULL);
     struct tm tm;
-    atomicGet(server.daylight_active, daylight_active);
-    nolocks_localtime(&tm,tv.tv_sec,server.timezone,daylight_active);
+    nolocks_localtime(&tm,tv.tv_sec,getTimeZone(),1);
     strftime(buf,sizeof(buf),"%d %b %Y %H:%M:%S.",&tm);
     printf("%s: AUTH BLOCK THREAD\n", buf);
     fflush(stdout);
@@ -154,7 +221,7 @@ void *AuthBlock_ThreadMain(void *arg) {
 
     gettimeofday(&tv,NULL);
     atomicGet(server.daylight_active, daylight_active);
-    nolocks_localtime(&tm,tv.tv_sec,server.timezone,daylight_active);
+    nolocks_localtime(&tm,tv.tv_sec,getTimeZone(),daylight_active);
     strftime(buf,sizeof(buf),"%d %b %Y %H:%M:%S.",&tm);
     printf("%s: AUTH BLOCK THREAD ABORT unblocked!!!!\n", buf);
     fflush(stdout);
