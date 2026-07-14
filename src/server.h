a96efaed9e31152ec51ebf6db2dbdfa6de4cd52f
Mincho Paskalev <minchopaskal@gmail.com>
2026-07-14 12:09:16 +0300

    small refactor

--- src/server.h
+++ src/server.h
@@ -3479,6 +3479,7 @@ void replicationFeedMonitors(client *c, list *monitors, int dictid, robj **argv,
 void updateSlavesWaitingBgsave(int bgsaveerr, int type);
 void replicationCron(void);
 void setReplCompression(int level);
+void enableMasterClientDecompression(client *c);
 void replicationStartPendingFork(void);
 void replicationHandleMasterDisconnection(void);
 void replicationCacheMaster(client *c);
